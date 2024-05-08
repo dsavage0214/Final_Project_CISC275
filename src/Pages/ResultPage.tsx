3e63f5ad05e1922613d22e5a26bf4a1deaba81de
stephen <roclame12@gmail.com>
2024-05-08 00:18:53 -0400

    in index.tsx: Turned off React.strict mode (This should 100% be turned back on at some point, I've just turned it off temporarily to stop the site from re-rendering at ResultPage.tsx
    in ResultPage.tsx:
    Changed <ResultAccordion/> to make placeholder accordion folds while the site receives results from the API
    
    Got rid of genReport() as its now obsolete
    
    in ResultPage.css:
    Added some CSS to give the placeholder headers animations to make it clear that those folds are loading

--- src/Pages/ResultPage.tsx
+++ src/Pages/ResultPage.tsx
@@ -1,23 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { find_key } from "../Components/Footer";
 import OpenAI from "openai";
-import "../CSS/ReportScreen.css";
+import "../CSS/ResultPage.css";
 import { Accordion } from "react-bootstrap";
 import LoadingPage from "./LoadingPage";
 import {useLocation} from "react-router-dom";
-import {safeJSON} from "openai/core";
-import {threadId} from "node:worker_threads";
 
+
+const NUM_JOBS = 10; // the amount of jobs you'd want the API to suggest
 let apiKey: string | undefined = find_key();
-
 // Initialize OpenAI API
 const openai = new OpenAI({ apiKey: apiKey, dangerouslyAllowBrowser: true });
-
-// these two interfaces are needed to pass the JSON that GPT generates without getting errors
-interface rootJson extends Object {
-  careers: Array<JsonParam>; // see jsonParam for what this is
-}
-
+// The JSON format that the API outputs
 interface JsonParam extends Object {
   job: string; // the name of the job
   description: string; // what you'd do in the job
@@ -26,43 +20,47 @@ interface JsonParam extends Object {
   orgs: Array<string>; // what company, charity, government, ect. hires people in this job
 }
 
+/**
+ * Takes the JSON report given by the API request to OpenAPI and then formats it into a Bootstrap accordion
+ * @author: Stephen Sayers
+ */
 function ResultAccordion({
-  GPTReport,
+  GPTReport, numEntries
 }: {
-  GPTReport: rootJson | undefined;
+  GPTReport: Array<JsonParam> | undefined,
+  numEntries: number;
 }): JSX.Element {
   /**
-   * Takes the JSON report given by the API request to OpenAPI and then formats it into a Bootstrap accordion
+   * makes the body of each accordion fold. Used as a callback function in the return of ResultAccordion function
+   * Should be used with Array.prototype.map
    * @author: Stephen Sayers
+   *
+   * @param json_object - an element from report
+   * @param i - the index for the accordion fold, used to make a unique id for the accordion fold
    */
-
   function makeAccordionBody(json_object: JsonParam, i: number) {
-    /**
-     * makes the body of each accordion fold. Used as a callback function in the return of ResultAccordion function
-     * @author: Stephen Sayers
-     */
     return (
       <Accordion.Item eventKey={i.toString()}>
-        <Accordion.Header>{json_object.job}</Accordion.Header>
+        <Accordion.Header><h3 className="accordion-header">{json_object.job}</h3></Accordion.Header>
         <Accordion.Body>
           <p>
-            <b>What is this job:</b> {json_object.description}
+            <b>Job description:</b> {json_object.description}
           </p>
-          <br />
+          <br/>
           <p>
             <b>Why it's a good fit:</b> {json_object.justification}
           </p>
-          <br />
+          <br/>
           <p>
             <b>training/education needed:</b> {json_object.training}
           </p>
-          <br />
+          <br/>
           <p>
             <b>Organizations in the field:</b>{" "}
             {json_object.orgs.reduce(
-              (print_string: string, org: string) =>
-                print_string + (org + ", "),
-              ""
+                (print_string: string, org: string) =>
+                    print_string + (org + ", "),
+                ""
             )}
           </p>
         </Accordion.Body>
@@ -70,9 +68,31 @@ function ResultAccordion({
     );
   }
 
+  /**
+   * Makes an Accordion fold with the same styling as makeAccordionBody except with placeholders
+   * @author Stephen Sayers
+   *
+   * @param i - the index of the accordion fold. used to make a unique id for the fold
+   */
+  function makePlaceholderAccordionBody(i: number): JSX.Element {
+    return(
+        <Accordion.Item eventKey={i.toString()}>
+          <Accordion.Header>
+            <h3 className="placeholder-header">Loading...</h3>
+          </Accordion.Header>
+        </Accordion.Item>
+    )
+  }
+
+  const placeholders: Array<JSX.Element> = []
+  for (let i = numEntries; i < NUM_JOBS; i++) {
+    placeholders.push(makePlaceholderAccordionBody(i))
+  }
+
   return (
     <Accordion alwaysOpen={true}>
-      {GPTReport?.careers.map(makeAccordionBody)}
+      {GPTReport?.map(makeAccordionBody)}
+      {placeholders}
     </Accordion>
   );
 }
@@ -81,9 +101,17 @@ export default function ResultPage(): React.JSX.Element {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [report, setReport] = useState<Array<JsonParam>>();
   const location = useLocation()
-  const assistantID: string = "asst_BkCqfCEjPOnW3Z3X0ePsamu8";
+  const assistantID: string = "asst_BkCqfCEjPOnW3Z3X0ePsamu8"; // assistant was made in advance in the OpenAI playground
+  const [numEntries, setNumEntries] = useState<number>(0)
 
   useEffect(() => {
+    /**
+     * Helper function to genAssistReport. Gets the thread's messages from OpenAI, collects GPT's responses, and then
+     * updates the state of report
+     * @author Stephen Sayers
+     *
+     * @param run - the current run from genAssistReport
+     */
     async function resolveRun(run: OpenAI.Beta.Threads.Runs.Run) {
       console.log("In resolveRun")
       let messages = await openai.beta.threads.messages.list(run.thread_id)
@@ -94,11 +122,17 @@ export default function ResultPage(): React.JSX.Element {
         }
       }
       setReport(report)
+      setNumEntries(report.length)
       console.log(report)
     }
 
-    async function assistReport() {
-        const results: string = location.state;
+    /**
+     * Generates a career report using OpenAI's assistants API. Is able to generate the report one career at a time to
+     * lower perceived load times compared to genReport
+     * @author Stephen Sayers
+     */
+    async function genAssistReport() {
+      const results: string = location.state;
 
         // split up the prompt into variables to make it easier to understand what's being asked of the API
         const job_str: string = "What's the best career for the user based off of these results? Make the JSON key for" +
@@ -128,9 +162,10 @@ export default function ResultPage(): React.JSX.Element {
             {assistant_id: assistantID, additional_instructions: "do not use line breaks in your response"}
         )
         if (run.status === "completed") {
-          resolveRun(run);
+          await resolveRun(run);
+          setIsLoading(false);
 
-          for (let i = 0; i < 9; i++) {
+          for (let i = 0; i < NUM_JOBS - 1; i++) {
             await openai.beta.threads.messages.create(
                 thread.id,
                 {
@@ -143,7 +178,7 @@ export default function ResultPage(): React.JSX.Element {
                 thread.id,
                 {assistant_id: assistantID, additional_instructions: "do not use line breaks in your response"}
             )
-            if (run.status === "completed") {resolveRun(run)}
+            if (run.status === "completed") {await resolveRun(run)}
             else {
               console.log(run.status)
               if (run.status === "failed") {console.log(run.last_error)}
@@ -155,86 +190,13 @@ export default function ResultPage(): React.JSX.Element {
           if (run.status === "failed") {console.log(run.last_error)}
         }
       }
-
-    /*
-    async function gen_report() {
-      /**
-       * Calls the OpenAI API to generate a JSON file containing ChatGPT's career suggestions, should only be called
-       * in the useEffect hook
-       * @author Stephen Sayers
-       */
-    /*
-    const sys_role: string =
-          "You're the reviewer of the results of a career test. You'll provide people the best possible suggestions for " +
-          "the career someone should take based off of the results of the test. Responses should be in JSON format where " +
-          "each career suggested should be its own JSON object, the root key for the JSON should be titled \"careers\"." +
-          "\n\nThe results of the test are the following:\n\n";
-      const results: string = location.state;
-
-      const report = await openai.chat.completions.create({
-        model: "gpt-4-turbo",
-        messages: [
-          { role: "system", content: sys_role + results }, // GPT's role and the test results
-          {
-            role: "assistant",
-            content:
-              'What\'s the 10 best careers for the test taker? Make the JSON key for each career "job"',
-          },
-          {
-            role: "user",
-            content:
-              "For each career, write a 3 sentence paragraph explaining what that job entails. Make the JSON key for " +
-               "each description \"description\"",
-          },
-          {
-            role: "user",
-            content:
-              "For each career, Explain why the job is a good fit for the test taker. Respond as if you " +
-              "were speaking to the test taker and, Make the JSON key for each explanation \"justification\"",
-          },
-          {
-            role: "user",
-            content:
-              "For each career, what training or education is needed? Please make the JSON key for each " +
-              "explanation \"training\""
-          },
-          {
-            role: "user",
-            content:
-              "For each career, list a couple of organizations that would hire in that field? Make the " +
-              "JSON key for each explanation \"orgs\"",
-          },
-        ],
-        max_tokens: 4096, // was able to generate pretty good results in the playground with this length
-        response_format: { type: "json_object" }, // will probably be easier to handle than a string
-      });
-      console.log("GPT finished due to: " + report.choices[0].finish_reason); // to see if GPT needs more tokens
-      console.log("JSON generated:" + report.choices[0].message.content);
-      if (report.choices[0].message.content !== null) {
-        setReport(JSON.parse(report.choices[0].message.content));
-        setIsLoading(false);
-      } else {
-        await gen_report();
-      } // the message is null, API call failed and needs to be done again
-      
-    }
-    gen_report();
-  */
-  assistReport()
+  genAssistReport()
   }, []);
-
-  const stop_errors: rootJson = {careers: [{
-    job: "test",
-    description: "test", // what you'd do in the job
-    justification: "test", // why GPT picked this job for the user
-    training: "test", // what degree, certification, ect you'd need for the job
-    orgs: ["test"] // what company, charity, government, ect. hires people in this job
-  }]}
 
   return (
     <div className={"placeholder-container"}>
       <h2>Test Completed!</h2>
-      {isLoading ? <LoadingPage /> : <ResultAccordion GPTReport={stop_errors} />}
+      {isLoading ? <LoadingPage /> : <ResultAccordion GPTReport={report} numEntries={numEntries}/>}
     </div>
   );
 }
